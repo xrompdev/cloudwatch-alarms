0c47bbd55b54db6e8243e9331bf7d715df4bf8e5
xromp.dev <xromp.dev@gmail.com>
2023-03-17 15:13:11 +0800

    feat: add alarm bot for slack

---
+++ config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  kmsEncryptedHookUrl: process.env.KMS_ENCRYPTED_HOOK_URL, // encrypted slack webhook url
+  unencryptedHookUrl: process.env.UNENCRYPTED_HOOK_URL,    // unencrypted slack webhook url
+  slackHookUrl: process.env.SLACK_HOOK_URL,    // unencrypted slack webhook url
+  discordHookUrl: process.env.DISCORD_HOOK_URL,    // unencrypted slack webhook url
+
+  services: {
+    elasticbeanstalk: {
+      // text in the sns message or topicname to match on to process this service type
+      match_text: "ElasticBeanstalkNotifications"
+    },
+    cloudwatch: {     
+    },
+    codepipeline: {
+      // text in the sns message or topicname to match on to process this service type
+      match_text: "CodePipelineNotifications"
+    },
+    codedeploy: {
+      // text in the sns message or topicname to match on to process this service type
+      match_text: "CodeDeploy"
+    },
+    elasticache: {
+      // text in the sns message or topicname to match on to process this service type
+      match_text: "ElastiCache"
+    },
+    autoscaling: {
+      // text in the sns message or topicname to match on to process this service type
+      match_text: "AutoScaling"
+    }
+  }
+
+}
